8b0318583a15a79429d8fc3e213e566da689db63
c <0.0@asmr.software>
2024-03-08 11:31:12 -0500

    begin v2

---
+++ v2/app/main.py
@@ -0,0 +1,8 @@
+import webview
+
+class Api():
+  def log(self, value):
+    print(value)
+
+webview.create_window("Test", html="<button onclick='pywebview.api.log(\"Woah dude!\")'>Click me</button>", js_api=Api())
+webview.start()
